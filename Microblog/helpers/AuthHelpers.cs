65f44992aa945ddc7ff8b7bf7cefcac9b53a6628
Timofey-Vodyanov <timofeyvodyanov@gmail.com>
2024-12-22 21:34:25 +0300

    created a function that generates a web token

---
+++ Microblog/helpers/AuthHelpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
+
+public class AuthHelpers
+{
+    private readonly IConfiguration _configuration;
+
+    // Конструктор для инициализации конфигурации
+    public AuthHelpers(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // Метод для генерации JWT токена
+    public string GenerateJWTToken(SystemUser user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Name),
+        };
+
+        // Добавляем роли в утверждения
+        if (user.Roles != null)
+        {
+            foreach (var role in user.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+        }
+
+        // Создание токена
+        var jwtToken = new JwtSecurityToken(
+            claims: claims,
+            notBefore: DateTime.UtcNow,
+            expires: DateTime.UtcNow.AddDays(30),
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["ApplicationSettings:JWT_Secret"])
+                ),
+                SecurityAlgorithms.HmacSha256Signature)
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+    }
+}
